9c2a663fb428976c7f26440bb397fd18a8e50c26
raveve <raven.askew@gmail.com>
2015-06-25 11:48:02 -0400

    Editing works properly now. Having issues showing the input on dblclick for all tasks. Working on that next.

--- jQuery.js
+++ jQuery.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function(){
 			//Do nothing (keeps from creating an empty item)
 		}
 		else {
-			jQuery('#tasks').append('<article data-itemid="' + uid + '"><h2><i class="fa fa-check-circle"></i></h2><a href="" class="delete-item"><h2><i class="fa fa-times-circle"></i></h2></a><div class="task"><h2 class="item dbl-click">' + newItem.item + '</h2></div><form class="edit-item edit-form" action=""><input type="text" name="editItem" value="' + newItem.item + '"></form></article>'),
+			jQuery('#tasks').append('<article data-itemid="' + uid + '"><h2><i class="fa fa-check-circle"></i></h2><a href="" class="delete-item"><h2><i class="fa fa-times-circle"></i></h2></a><div class="task"><h2 class="item dbl-click">' + newItem.item + '</h2><form class="edit-item edit-form" action=""><input type="text" name="editItem" value="' + newItem.item + '"></form></div></article>'),
 			localStorage.setItem(uid, JSON.stringify(newItem)),
 			jQuery('.form-create input').val('');
 			jQuery('article').on('dblclick', '.dbl-click', function(event){
@@ -36,7 +36,7 @@ jQuery(document).ready(function(){
 	jQuery('section').on('click', '.fa-check-circle', function(event) {
 		console.log('check-circle click works'); // Remove later
 		var completed = jQuery(this).parent().siblings().children('h2');
-		jQuery(completed).addClass('completed'); // toggleClass might be an option, see commented out code belowbelow
+		jQuery(completed).addClass('completed'); // toggleClass might be an option, see commented out code below
 		if(jQuery('.completed')) {
 			var itemId = jQuery(this).closest('article').data('itemid');
 			localStorage.setItem(itemId, JSON.stringify({
@@ -55,27 +55,17 @@ jQuery(document).ready(function(){
 		// }
 	});
 
-	// jQuery('article').on('dblclick', '.dbl-click', function(event){
-	// 	event.preventDefault();
-	// 	console.log("Double click is doing something"); // Remove later
-	// 	jQuery(this).hide();
-	// 	jQuery(this).closest('article').find('form').toggleClass('edit-form');
-	// });
-
 	jQuery('section').on('submit', '.edit-item', function(event) {
 		event.preventDefault();
 		jQuery('.dbl-click').show();
 		jQuery(this).toggleClass('edit-form');
 		var itemId = jQuery(this).closest('article').data('itemid');
-		// Tried using the below instead of what I have in the setItem below and replaceWith, gave me an undefined instead
-		// var editedItem = jQuery(this).find('.edit-item input').val();
 		var taskObject = JSON.parse(localStorage.getItem(itemId));
 		localStorage.setItem(itemId, JSON.stringify({
 			id: itemId,
-			item: jQuery('section').find('.edit-item input').val(),
+			item: jQuery(this).find('input[name="editItem"]').val(),
 			complete: taskObject.complete
 		}));
-		jQuery(this).siblings('.task').children('h2').replaceWith('<h2>' + jQuery('section').find('.edit-item input').val()  + '</h2>');
+		jQuery(this).parent('.task').children('h2').replaceWith('<h2 class="item dbl-click">' + jQuery(this).find('input[name="editItem"]').val() + '</h2>');
 	});
-
 });
